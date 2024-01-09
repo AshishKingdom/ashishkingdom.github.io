3c841f5179d0d85c6e3a809b074c751ae11bd4e0
Ashish Kushwaha <ashishkushwahacb@gmail.com>
2024-01-09 13:54:38 +0530

    add icpc data

--- js/achievements.js
+++ js/achievements.js
@@ -10,15 +10,20 @@ const achievements = [
       "Participated in Amazon HackOn 3.0 and secured a position in top 50 out of 50K registrations.",
   },
   {
+    title: "ICPC 2023-24 Regionalist",
+    description:
+      "Qualified ICPC Regionals round for Amritapuri and Chennai regionals.",
+  },
+  {
     title: "3-star rated coder on Codechef",
     description:
       "Solved 100+ problems on Codechef and secured a rating of 1722 (AIR 6122).<br> <a class='link' href='https://www.codechef.com/users/ashishkingdom'>Codechef Profile</a>",
   },
-  {
-    title: "Max Rating of Pupil on Codeforces",
-    description:
-      "Solved 100+ problems on Codeforces and secured a rating of 1250 (Max Rating of Pupil).<br> <a class='link' href='https://codeforces.com/profile/ashishkingdom'>Codeforces Profile</a>",
-  },
+  // {
+  //   title: "Max Rating of Pupil on Codeforces",
+  //   description:
+  //     "Solved 100+ problems on Codeforces and secured a rating of 1250 (Max Rating of Pupil).<br> <a class='link' href='https://codeforces.com/profile/ashishkingdom'>Codeforces Profile</a>",
+  // },
   {
     title: "Completed Hacktoberfest 2022",
     description:
